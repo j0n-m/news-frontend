e533461d0f8ab94d677f7a5352aa7ffa320b4a21
Jon Monarrez <135302142+j0n-m@users.noreply.github.com>
2024-11-14 19:35:50 -0600

    initialize project

---
+++ src/types/feed.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+
+export const FeedItemSchema = z.object({
+  content: z.string(),
+  image_url: z.string(),
+  title: z.string(),
+  content_snippet: z.string(),
+  pubDate: z.coerce.date(),
+  id: z.number(),
+  source_link: z.string(),
+  author: z.string().optional(),
+});
+
+export const FeedSchema = z.object({
+  feed_title: z.string(),
+  feed_link: z.string(),
+  feed_description: z.string().optional(),
+  items: z.array(FeedItemSchema),
+});
+
+export const FeedResponseSchema = z.object({
+  data: z.array(FeedSchema),
+  nextStart: z.number(),
+});
+export const NextStartSchemaBySource = z.object({
+  skip: z.number(),
+  limit: z.number(),
+});
+export const FeedResponseBySourceSchema = z.object({
+  data: z.array(FeedSchema),
+  nextStart: NextStartSchemaBySource,
+});
+
+export type FeedResponse = z.infer<typeof FeedResponseSchema>;
+export type FeedResponseBySource = z.infer<typeof FeedResponseBySourceSchema>;
+export type Feed = z.infer<typeof FeedSchema>;
+export type FeedItem = z.infer<typeof FeedItemSchema>;
