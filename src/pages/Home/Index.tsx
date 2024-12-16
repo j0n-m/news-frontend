e401338fc5e0daef20a19071659b91bdb5a37543
Jon Monarrez <135302142+j0n-m@users.noreply.github.com>
2024-12-15 18:00:38 -0600

    finish styling index page

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -3,8 +3,12 @@ import Nav from "../../components/NavigationBar/Nav";
 import feedSrc from "../../assets/images/feed-sources.png";
 import website from "../../assets/images/website.png";
 import favorites from "../../assets/images/favorites.png";
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Footer from "@/components/Footer";
+import f1 from "../../assets/images/f1.png";
+import f2 from "../../assets/images/f2.png";
+import f3 from "../../assets/images/f3.png";
+import { Link } from "@tanstack/react-router";
 
 function Index() {
   return (
@@ -26,8 +30,9 @@ function Index() {
                 <Button
                   variant={"blue"}
                   className="py-6 px-8 text-lg hover:scale-105 transition-all duration-300"
+                  asChild
                 >
-                  Get started for free
+                  <Link to="/signup">Get started for free</Link>
                 </Button>
               </div>
             </div>
@@ -67,25 +72,48 @@ function Index() {
                 <img
                   src={favorites}
                   alt="screenshot"
-                  className="-order-1 md:max-w-[400px]"
+                  className="md:-order-1 md:max-w-[400px]"
                 />
               </div>
             </div>
 
             <div className="testimonials py-24">
-              <div className="flex flex-col justify-center items-center gap-10">
-                <h2 className="text-4xl font-semibold text-center">
+              <div className="flex flex-col justify-center items-center gap-14">
+                <h2 className="text-4xl font-semibold text-center mb-4">
                   Loved by millions of readers
                 </h2>
 
-                <div className="testimonial-card flex flex-col justify-center gap-4 items-center lg:flex-row mt-6">
-                  <Avatar className="size-12">
+                <div className="testimonial-card flex flex-col justify-center gap-4 items-center lg:flex-row">
+                  <Avatar className="size-20 lg:size-16">
+                    <AvatarImage
+                      src={f3}
+                      className="object-cover"
+                    ></AvatarImage>
+                    <AvatarFallback className="">A</AvatarFallback>
+                  </Avatar>
+                  <div className="testimonial-content text-center lg:text-start lg:max-w-[525px]">
+                    <p className="t-title text-xl font-semibold">
+                      Anna James, National Readers Association
+                    </p>
+                    <p className="t-comment mt-2 opacity-60">
+                      News RSS keeps me updated on the lastest news and easy to
+                      use, the best of both worlds!
+                    </p>
+                  </div>
+                </div>
+
+                <div className="testimonial-card flex flex-col justify-center gap-4 items-center lg:flex-row">
+                  <Avatar className="size-20 lg:size-16">
+                    <AvatarImage
+                      src={f1}
+                      className="object-cover"
+                    ></AvatarImage>
                     <AvatarFallback className="">JF</AvatarFallback>
                   </Avatar>
 
                   <div className="testimonial-content text-center lg:text-start lg:max-w-[525px]">
                     <p className="t-title text-xl font-semibold">
-                      Jake Farms, The Readers Group
+                      Jake Farms, Reader's Lounge
                     </p>
                     <p className="t-comment mt-2 opacity-60">
                       I love how I can access all my news in one place with News
@@ -93,17 +121,22 @@ function Index() {
                     </p>
                   </div>
                 </div>
+
                 <div className="testimonial-card flex flex-col justify-center gap-4 items-center lg:flex-row">
-                  <Avatar className="size-12">
+                  <Avatar className="size-20 lg:size-16">
+                    <AvatarImage
+                      src={f2}
+                      className="object-cover"
+                    ></AvatarImage>
                     <AvatarFallback className="">LD</AvatarFallback>
                   </Avatar>
                   <div className="testimonial-content text-center lg:text-start lg:max-w-[525px]">
                     <p className="t-title text-xl font-semibold">
-                      Luke Duke, Race car Magazine
+                      Luke Duke, News Paper Weekly
                     </p>
                     <p className="t-comment mt-2 opacity-60">
-                      News RSS is easy to use and not sure how I haven't
-                      discovered this sooner!
+                      News RSS is simple, yet impressive news aggregator app
+                      that's tailored to us. Wish I found this sooner.
                     </p>
                   </div>
                 </div>
